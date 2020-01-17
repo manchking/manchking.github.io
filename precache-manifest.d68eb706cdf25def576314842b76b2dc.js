bcad2b0618a628d32e12b40a485f91f14a67f32d
manchking <manchking@yandex.ru>
2020-01-18 02:10:26 +0400

    first rel

---
+++ precache-manifest.d68eb706cdf25def576314842b76b2dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802682c93f50e467278954345b3ca339",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1ba5b068fe77c193fae",
+    "url": "/static/css/main.56f4cf56.chunk.css"
+  },
+  {
+    "revision": "7f547084561dbf1e3fa2",
+    "url": "/static/js/2.e85eb6df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e85eb6df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1ba5b068fe77c193fae",
+    "url": "/static/js/main.8d5664be.chunk.js"
+  },
+  {
+    "revision": "b39c5e1d937db26be407",
+    "url": "/static/js/runtime-main.03e9c318.js"
+  },
+  {
+    "revision": "e2753e6318bbe8d5c2eb4b4b1633438e",
+    "url": "/static/media/remove.e2753e63.png"
+  }
+]);
